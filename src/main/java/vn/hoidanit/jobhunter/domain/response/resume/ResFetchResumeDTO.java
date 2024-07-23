645831868848e359ee3659d485edb1a39d5adba1
kira <thanh0919234514@gmail.com>
2024-07-23 12:46:48 +0700

    permision & role

--- src/main/java/vn/hoidanit/jobhunter/domain/response/resume/ResFetchResumeDTO.java
+++ src/main/java/vn/hoidanit/jobhunter/domain/response/resume/ResFetchResumeDTO.java
@@ -19,6 +19,7 @@ public class ResFetchResumeDTO {
     private String createdBy;
     private String updatedBy;
 
+    private String companyName;
     private UserResume user;
     private JobResume job;
 
@@ -154,6 +155,14 @@ public class ResFetchResumeDTO {
 
     public void setJob(JobResume job) {
         this.job = job;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
     }
 
 }
